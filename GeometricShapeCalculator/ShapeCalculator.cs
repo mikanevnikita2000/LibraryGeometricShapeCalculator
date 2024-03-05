7e1a82e6686d479e25791c2937da8b87ea3f0706
mikanevnikita2000 <86831041+mikanevnikita2000@users.noreply.github.com>
2024-03-05 16:57:47 +0300

    Добавьте файлы проекта.

---
+++ GeometricShapeCalculator/ShapeCalculator.cs
@@ -0,0 +1,23 @@
+﻿namespace GeometricShapeCalculator
+{
+    public class ShapeCalculator
+    {
+        public double CalculateCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public double CalculateTriangleArea(double side1, double side2, double side3)
+        {
+            double semiPerimeter = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
+        }
+
+        public bool IsRightTriangle(double side1, double side2, double side3)
+        {
+            double[] sides = { side1, side2, side3 };
+            Array.Sort(sides);
+            return sides[0] * sides[0] + sides[1] * sides[1] == sides[2] * sides[2];
+        }
+    }
+}
