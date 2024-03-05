79094a3180387a48bd05b23453c5527951b0aebf
mikanevnikita2000 <86831041+mikanevnikita2000@users.noreply.github.com>
2024-03-05 17:04:45 +0300

    version1

--- GeometricShapeCalculator/ShapeCalculator.cs
+++ GeometricShapeCalculator/ShapeCalculator.cs
@@ -2,12 +2,12 @@
 {
     public class ShapeCalculator
     {
-        public double CalculateCircleArea(double radius)
+        public double CircleArea(double radius)
         {
             return Math.PI * radius * radius;
         }
 
-        public double CalculateTriangleArea(double side1, double side2, double side3)
+        public double TriangleArea(double side1, double side2, double side3)
         {
             double semiPerimeter = (side1 + side2 + side3) / 2;
             return Math.Sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
